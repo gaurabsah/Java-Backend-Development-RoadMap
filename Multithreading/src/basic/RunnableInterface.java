030c0355f9c436b0ed3315874ec4962191afacc1
Gaurab Sah <gaurab.sah.777@gmail.com>
2024-05-29 15:22:10 +0530

    Runnable Interface

--- Multithreading/src/basic/RunnableInterface.java
+++ Multithreading/src/basic/RunnableInterface.java
@@ -1,5 +1,7 @@
 package basic;
 
+// Runnable mechanism is recommended to define a Thread.
+
 class B implements Runnable {
 
 	@Override
@@ -15,9 +17,20 @@ class B implements Runnable {
 
 public class RunnableInterface {
 	public static void main(String[] args) {
-		
+
 		B b = new B();
-		b.
+//		b.run(); // run as normal method
+		Thread t = new Thread();
+		t.start(); // new thread will be created and it will print only main method but not run method because run() is not overriden
+//		t.run(); // it will print only main method but not run method because run() is not overriden
+
+		Thread myThread = new Thread(b); // here b is target runnable
+//		myThread.start(); // new thread will be created
+		myThread.run(); // run as normal method and no new thread
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main Thread- : " + i);
+		}
 
 	}
 
