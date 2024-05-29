870b550ffbbc0817d1439acf9a84c971423374fe
Gaurab Sah <gaurab.sah.777@gmail.com>
2024-05-29 14:51:09 +0530

    Thread Class

---
+++ Multithreading/src/basic/RunnableInterface.java
@@ -0,0 +1,24 @@
+package basic;
+
+class B implements Runnable {
+
+	@Override
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Thread-inside Run() : " + i);
+		}
+
+	}
+
+}
+
+public class RunnableInterface {
+	public static void main(String[] args) {
+		
+		B b = new B();
+		b.
+
+	}
+
+}
--- Multithreading/src/basic/ThreadClass.java
+++ Multithreading/src/basic/ThreadClass.java
@@ -1,11 +1,31 @@
 package basic;
 
 class A extends Thread {
+	
+//	always override run methods otherwise don't go for multithreading
 	public void run() {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Child Thread : " + i);
+			System.out.println("Child Thread :no-args " + i);
 		}
 	}
+
+	
+//	it will not execute as thread class doesn't contains parameterized run method.
+	public void run(int t) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Thread :with-args " + i);
+		}
+	}
+	
+//	if you override start method of thread class then
+//	it will be executed as normal methods and no thread will be created
+	
+	public void start() {
+		System.out.println("Start method");
+	}
+	
+	
+	
 }
 
 public class ThreadClass {
@@ -21,6 +41,8 @@ public class ThreadClass {
 		 * output depends upon JVM (Thread Scheduler:- which thread will execute first
 		 * is decided by Thread Scheduler)
 		 */
+		
+//		System.out.println("Main Method");
 
 	}
 
